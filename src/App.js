8f9a53b1d83f592cd4b4a60793bc6c5f3d1c4231
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-07-31 07:23:37 +0300

    create the first component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <div className="component">
-      <h1>App Component</h1>
-    </div>
+      <div className="component"></div>
     </div>
   );
 }
