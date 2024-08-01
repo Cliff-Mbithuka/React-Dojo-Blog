c30bca6787ca44e19c3951a5b622cb81c4d93e9a
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-08-01 06:50:55 +0300

    Add click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+
+  const handleClick = () => {
+    console.log('Hello Cliff');
+  }
+  const handleClickAgain = (name) => {
+    console.log('Hello ' + name);
+  }
     return ( 
        <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={() => handleClickAgain('mario')}>Click me Again</button>
        </div>
      );
 }
