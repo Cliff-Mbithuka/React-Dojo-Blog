6887670ea13a90eac547782e301543f82fbc5a84
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-08-01 06:12:39 +0300

    Add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <div className="component"></div>
+      <Navbar/>
+      <div className="component">
+      <Home/>
+      </div>
     </div>
   );
 }
