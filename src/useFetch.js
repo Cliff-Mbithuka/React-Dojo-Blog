79044572e2bc45f8ae7c87789490306f3c86597e
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-08-06 16:17:41 +0300

    UseEffect clean up

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 
-const useFetch= (url) => {
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-  
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error("could not fetch the data for that resourse");
@@ -20,16 +22,19 @@ const useFetch= (url) => {
           setError(null);
         })
         .catch((err) => {
-          setIsPending(false);
-         setError(err.message)
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
 
-    return () => console.log('cleanup');
-    
+    return () => abortCont.abort();
   }, [url]);
 
-  return {data, isPending, error}
-}
+  return { data, isPending, error };
+};
 
 export default useFetch;
