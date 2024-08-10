1c211312509bb60378bf6e680c29fe02634a44e2
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-08-10 15:31:20 +0300

    post request

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,13 @@ const Create = () => {
 const[title, setTitle] = useState('');
 const[body, setBody] = useState('');
 const [author, setAuthor] = useState('');
+const [isPending, setIspending] = useState(false)
 
 const handleSubmit = (e) => {
   e.prevent.Default();
-  const blog = {title, body, author}
+  const blog = {title, body, author};
+
+  setIspending(true)
 
   fetch('http://localhost:2485/blogs', {
     method: 'POST',
@@ -15,7 +18,7 @@ const handleSubmit = (e) => {
     body: JSON.stringify(blog)
   }).then(() => {
     console.log("new blog added");
-    
+    setIspending(false)
   })
 }
 
@@ -32,17 +35,14 @@ const handleSubmit = (e) => {
           onChange= {(e) => setBody(e.target.value)}>
           </textarea>
 
-
         <label>Blog author: </label>
         <select value="author"
         onChange= {(e) => setAuthor(e.target.value)}>
           <option value="mario" ></option>
           <option value="yoshi"></option>
         </select>
-        <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        {!isPending && <button>Add Blog</button>}
+        {!isPending && <button disabled>Adding Blog ...</button>}
       </form>
     </div>
   );
